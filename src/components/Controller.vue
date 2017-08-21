4c67c403ce925fd4ebb172bd0ca440d61fbdd422
chenqipeng <chenqipeng.cqp@gmail.com>
2017-08-21 15:52:56 +0800

    新增进度条

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,19 +1,28 @@
 <template lang="html">
   <div id="controller">
-    <div class="column">
-      <span class="iconfont icon-liebiaoxunhuan"></span>
+    <div class="progress-control">
+      <div class="current-time">00:52</div>
+      <div class="progress-bar">
+        <div class="current-point"></div>
+      </div>
+      <div class="total-time">05:00</div>
     </div>
-    <div class="column">
-      <span class="iconfont icon-shangyishou"></span>
-    </div>
-    <div class="column">
-      <span class="iconfont icon-zanting"></span>
-    </div>
-    <div class="column">
-      <span class="iconfont icon-xiayishou"></span>
-    </div>
-    <div class="column">
-      <span class="iconfont icon-bofangliebiao"></span>
+    <div class="play-control">
+      <div class="column">
+        <span class="iconfont icon-liebiaoxunhuan"></span>
+      </div>
+      <div class="column">
+        <span class="iconfont icon-shangyishou"></span>
+      </div>
+      <div class="column">
+        <span class="iconfont icon-zanting"></span>
+      </div>
+      <div class="column">
+        <span class="iconfont icon-xiayishou"></span>
+      </div>
+      <div class="column">
+        <span class="iconfont icon-bofangliebiao"></span>
+      </div>
     </div>
   </div>
 </template>
@@ -24,14 +33,57 @@ export default {
 </script>
 
 <style lang="css">
-#controller {
+.progress-control {
+  position: relative;
+  box-sizing: border-box;
+  height: 22px;
+  padding: 10px 60px;
+}
+
+.current-time, .total-time {
+  position: absolute;
+  top: 0;
+  width: 60px;
+  height: 100%;
+  text-align: center;
+  line-height: 25px;
+  font-size: 12px;
+}
+
+.progress-bar {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  background-color: #ccc;
+}
+
+.current-time {
+  left: 0;
+}
+
+.total-time {
+  right: 0;
+}
+
+.current-point {
+  position: absolute;
+  top: -6px;
+  left: -7px;
+  width: 14px;
+  height: 14px;
+  background-color: #000;
+  border-radius: 7px;
+}
+
+.play-control {
+  margin-top: 20px;
   font-size: 0;
 }
 
 .column {
   display: inline-block;
   width: 20%;
-  padding-top: 30px;
   text-align: center;
 }
 .column .iconfont {
