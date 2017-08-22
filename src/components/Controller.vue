ef57b009ea1618b4f5efca57c12b640a3338161d
chenqipeng <chenqipeng.cqp@gmail.com>
2017-08-22 12:10:19 +0800

    触控调节时间

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div id="controller">
     <div class="progress-control">
-      <div class="current-time">00:52</div>
-      <div class="progress-bar">
-        <div class="current-point"></div>
+      <div class="current-time">{{time}}</div>
+      <div class="progress-bar" @touchstart="touchstart" @touchmove="touchmove">
+        <div class="progress-line" id="progress-line"></div>
+        <div class="progress-point"></div>
       </div>
       <div class="total-time">05:00</div>
     </div>
@@ -29,6 +30,51 @@
 
 <script>
 export default {
+  data: function() {
+    return {
+      time: '00:00'
+    }
+  },
+  methods: {
+    touchstart (event) {
+      this.time = calTime(event.changedTouches[0].clientX);
+    },
+    touchmove (event) {
+      this.time = calTime(event.changedTouches[0].clientX);
+    }
+  }
+}
+
+/**
+ * 触点起始横坐标，值为进度条偏移值
+ * @type {Number}
+ */
+const BeginX = 60;
+
+/**
+ * 计算用户调节的时间
+ * @param  {Number} x 触点的clientX
+ * @return {String} “分:秒”
+ */
+function calTime(x) {
+  const TotalWidth = document.getElementById('progress-line').offsetWidth;
+  const TotalTime = 300;
+  let offsetX = (x>BeginX) ? Math.round(x-BeginX) : 0;
+  let currentX = (offsetX<TotalWidth) ? offsetX : TotalWidth;
+  let currentS = Math.round(currentX/TotalWidth*TotalTime);
+  return trsfTime(currentS);
+}
+
+/**
+ * 将秒数转换成“分:秒”的形式
+ * @param  {Number} second 秒数
+ * @return {String} “分:秒”
+ */
+function trsfTime(second) {
+  let m = Math.trunc(second/60);
+  let s = Math.trunc(second%60);
+  let format = (b) => (b>=10) ? b : ('0'+b);
+  return format(m) + ':' + format(s);
 }
 </script>
 
@@ -37,7 +83,7 @@ export default {
   position: relative;
   box-sizing: border-box;
   height: 22px;
-  padding: 10px 60px;
+  padding: 0px 60px;
 }
 
 .current-time, .total-time {
@@ -50,14 +96,6 @@ export default {
   font-size: 12px;
 }
 
-.progress-bar {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
-  background-color: #ccc;
-}
-
 .current-time {
   left: 0;
 }
@@ -66,9 +104,21 @@ export default {
   right: 0;
 }
 
-.current-point {
+.progress-bar {
+  position: relative;
+  box-sizing: border-box;
+  height: 100%;
+  padding: 10px 0px;
+}
+
+.progress-line {
+  height: 100%;
+  background-color: #ccc;
+}
+
+.progress-point {
   position: absolute;
-  top: -6px;
+  top: 4px;
   left: -7px;
   width: 14px;
   height: 14px;
