79d6abf68188f94278cb0e06be8c47ffd6f6a492
chenqipeng <chenqipeng.cqp@gmail.com>
2017-08-23 14:19:30 +0800

    更新进度条

--- src/components/Controller.vue
+++ src/components/Controller.vue
@@ -1,13 +1,17 @@
 <template lang="html">
   <div id="controller">
+    <audio id="audioPlayer" controls @canplay="canPlay" @play="play" @pause="pause" src="http://www.w3school.com.cn/i/song.mp3"></audio>
+
     <div class="progress-control">
-      <div class="current-time">{{time}}</div>
-      <div class="progress-bar" id="progress-bar" @touchstart="touchstart" @touchmove="touchmove">
-        <div class="progress-line" id="progress-line"></div>
-        <div class="progress-point" id="progress-point" @touchstart="touchstart"></div>
+      <div class="current-time">{{currentTime}}</div>
+      <div class="progress-bar" id="progress-bar" @touchstart="followTouch" @touchmove="followTouch" @touchend="updateTouch">
+        <div class="progress-line" id="progress-line">
+          <span class="progress-point"></span>
+        </div>
       </div>
-      <div class="total-time">05:00</div>
+      <div class="total-time">{{totalTime}}</div>
     </div>
+
     <div class="play-control">
       <div class="column">
         <span class="iconfont icon-liebiaoxunhuan"></span>
@@ -16,7 +20,7 @@
         <span class="iconfont icon-shangyishou"></span>
       </div>
       <div class="column">
-        <span class="iconfont icon-zanting"></span>
+        <span class="iconfont icon-bofang" id="toggle" @touchend="togglePop"></span>
       </div>
       <div class="column">
         <span class="iconfont icon-xiayishou"></span>
@@ -25,6 +29,7 @@
         <span class="iconfont icon-bofangliebiao"></span>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -32,58 +37,112 @@
 export default {
   data: function() {
     return {
-      time: '00:00'
+      currentTime: '00:00',
+      totalTime: '00:00'
     }
   },
   methods: {
-    touchstart (event) {
-      this.time = calTime(event.changedTouches[0].clientX);
+    followTouch (event) {
+      if(UpdateView.si !== 0) { //先停止自动更新视图
+        clearInterval(UpdateView.si);
+        UpdateView.si = 0;
+      }
+
+      const BeginX = 60; //触点起始横坐标，值为进度条偏移值
+      const TotalWidth = document.getElementById('progress-bar').offsetWidth;
+      const TotalTime = document.getElementById('audioPlayer').duration;
+      let progressLine = document.getElementById('progress-line');
+      let clientX = event.changedTouches[0].clientX;
+      let offsetX = (clientX>BeginX) ? clientX-BeginX : 0;
+      let currentX = (offsetX<TotalWidth) ? offsetX : TotalWidth;
+      let currentS = Math.round(currentX/TotalWidth*TotalTime);
+      progressLine.style.width = currentX + 'px';
+      this.currentTime = sToms(currentS);
     },
-    touchmove (event) {
-      this.time = calTime(event.changedTouches[0].clientX);
+    updateTouch () {
+      let vm = this;
+      let audio = document.getElementById('audioPlayer');
+      audio.currentTime = msTos(this.currentTime);
+      if(!audio.paused) { //恢复视图自动更新
+        UpdateView.si = setInterval(function() {
+          UpdateView.progressLine(audio.currentTime);
+          vm.currentTime = sToms(audio.currentTime);
+        }, UpdateView.TimeOut);
+      }
+    },
+    togglePop () {
+      let audio = document.getElementById('audioPlayer');
+      if(audio.paused) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    },
+    canPlay () {
+      this.totalTime = sToms(document.getElementById('audioPlayer').duration);
+    },
+    play () {
+      let vm = this;
+      let audio = document.getElementById('audioPlayer');
+      UpdateView.toggle();
+      UpdateView.si = setInterval(function() {
+        UpdateView.progressLine(audio.currentTime);
+        vm.currentTime = sToms(audio.currentTime);
+      }, UpdateView.TimeOut);
+    },
+    pause () {
+      let audio = document.getElementById('audioPlayer');
+      UpdateView.toggle();
+      UpdateView.progressLine(audio.currentTime);
+      this.currentTime = sToms(audio.currentTime);
+      clearInterval(UpdateView.si);
+      UpdateView.si = 0;
     }
   }
 }
 
 /**
- * 触点起始横坐标，值为进度条偏移值
- * @type {Number}
+ * 将's'形式转换成'mm:ss'
+ * @param  {Number} s 秒数
+ * @return {String} 'mm:ss'形式
  */
-const BeginX = 60;
-
-/**
- * 进度条点偏移量
- * @type {Number}
- */
-const PointOffset = 7;
-
-/**
- * 计算用户调节的时间
- * @param  {Number} x 触点的clientX
- * @return {String} “分:秒”
- */
-function calTime(x) {
-  const TotalWidth = document.getElementById('progress-bar').offsetWidth;
-  const TotalTime = 300;
-  let offsetX = (x>BeginX) ? Math.round(x-BeginX) : 0;
-  let currentX = (offsetX<TotalWidth) ? offsetX : TotalWidth;
-  document.getElementById('progress-point').style.left = (currentX-PointOffset) + 'px';
-  document.getElementById('progress-line').style.width = currentX + 'px';
-  let currentS = Math.round(currentX/TotalWidth*TotalTime);
-  return trsfTime(currentS);
-}
-
-/**
- * 将秒数转换成“分:秒”的形式
- * @param  {Number} second 秒数
- * @return {String} “分:秒”
- */
-function trsfTime(second) {
-  let m = Math.trunc(second/60);
-  let s = Math.trunc(second%60);
+function sToms(s) {
+  let min = Math.trunc(s/60);
+  let second = Math.trunc(s%60);
   let format = (b) => (b>=10) ? b : ('0'+b);
-  return format(m) + ':' + format(s);
+  return format(min) + ':' + format(second);
 }
+
+/**
+ * 将'mm:ss'形式转换成's'
+ * @param  {String} ms 'mm:ss'形式
+ * @return {Number} 秒数
+ */
+function msTos(ms) {
+  let msAry = ms.split(':');
+  return msAry[0]*60 + msAry[1];
+}
+
+/**
+ * 视图更新对象
+ * @type {Object}
+ */
+const UpdateView = {
+  si: 0, //setInterval intervalID
+  TimeOut: 500, //setInterval timeout
+  progressLine (currentTime) {
+    const TotalWidth = document.getElementById('progress-bar').offsetWidth;
+    const TotalTime = document.getElementById('audioPlayer').duration;
+    let currentX = currentTime/TotalTime * TotalWidth;
+    document.getElementById('progress-line').style.width = currentX + 'px';
+  },
+  toggle () {
+    let toggleEl = document.getElementById('toggle');
+    toggleEl.classList.toggle('icon-bofang');
+    toggleEl.classList.toggle('icon-zanting');
+  }
+};
+
 </script>
 
 <style lang="css">
@@ -113,7 +172,6 @@ function trsfTime(second) {
 }
 
 .progress-bar {
-  position: relative;
   box-sizing: border-box;
   height: 100%;
   padding: 10px 0px;
@@ -121,6 +179,7 @@ function trsfTime(second) {
 }
 
 .progress-line {
+  position: relative;
   width: 0;
   height: 100%;
   background-color: #f00;
@@ -128,8 +187,8 @@ function trsfTime(second) {
 
 .progress-point {
   position: absolute;
-  top: 4px;
-  left: -7px;
+  top: -6px;
+  right: -7px;
   width: 14px;
   height: 14px;
   background-color: #000;
